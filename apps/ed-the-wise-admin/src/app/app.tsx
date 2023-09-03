037d245b4dc5ffb74e8e99fa7296d77d06190d26
ageorge <ageorge@marchnetworks.com>
2023-09-03 00:05:30 -0400

    question entry form initialized

--- apps/ed-the-wise-admin/src/app/app.tsx
+++ apps/ed-the-wise-admin/src/app/app.tsx
@@ -1,8 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { QuestionEntryForm } from "@edthewise/application-admin-web";
 import styles from "./app.module.css";
 
 export function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <QuestionEntryForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ libs/application-admin-web/src/components/QuestionEntryForm.tsx
@@ -0,0 +1,28 @@
+export const QuestionEntryForm = (props: any) => {
+  return <div>Hello</div>;
+};
+
+/**
+ * This is a component that will be used to enter questions TO THE db
+ *
+ * These are the data fields that will be entered:
+ *
+ *
+ * QP1: string
+ * QP2: string
+ * QP3: string
+ * QP4: string
+ *
+ * QTable1: string[]
+ * QTable2: string[]
+ *
+ * QOptions: string[]
+ *
+ * year: string
+ *
+ * QAnswer: string
+ *
+ * QId: string
+ *
+ * QComponent_Order: string
+ */
