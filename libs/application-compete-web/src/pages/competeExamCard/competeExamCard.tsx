c0d7faabdb3ed837e523c9a8aa58f9cc4ad1f734
ageorge <ageorge@marchnetworks.com>
2023-09-23 13:05:11 -0400

    questions arrow-left-right navigation

--- libs/application-compete-web/src/pages/competeExamCard/competeExamCard.tsx
+++ libs/application-compete-web/src/pages/competeExamCard/competeExamCard.tsx
@@ -4,6 +4,7 @@ import { container } from "@edthewise/common-inversify";
 import { TOKENS } from "@edthewise/common-tokens-web";
 import { observer } from "mobx-react";
 import { useRouterStore } from "mobx-state-router";
+import { useEffect } from "react";
 
 export const CompeteExamCard = observer(() => {
   const routerStore = useRouterStore();
@@ -58,6 +59,22 @@ export const CompeteExamCard = observer(() => {
     goToNextQuestion: goToNextQuestion,
     goToPrevQuestion: goToPreviousQuestion,
   };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowLeft") {
+        goToPreviousQuestion(event);
+      } else if (event.key === "ArrowRight") {
+        goToNextQuestion(event);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
 
   return <ExamCard {...examCardProps} />;
 });
